5983b5b1361a22b45e8876f3949cc9e282e0706d
jakew-o-0 <jakewbailey007@gmail.com>
2023-12-02 02:55:07 +0000

    it starts

---
+++ day1/target/debug/day1.d
@@ -0,0 +1,1 @@
+/home/jake/code/Advent-Of-Code-2023/day1/target/debug/day1: /home/jake/code/Advent-Of-Code-2023/day1/src/main.rs
---
+++ day1/target/debug/deps/day1-1c601c97a7a57879.d
@@ -0,0 +1,5 @@
+/home/jake/code/Advent-Of-Code-2023/day1/target/debug/deps/day1-1c601c97a7a57879: src/main.rs
+
+/home/jake/code/Advent-Of-Code-2023/day1/target/debug/deps/day1-1c601c97a7a57879.d: src/main.rs
+
+src/main.rs:
